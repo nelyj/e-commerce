ba178815eaead968b9f7b636e44e4c008255e6c3
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-03-14 21:58:16 -0300

    Add font face non web

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ ECommerce::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf)
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ ECommerce::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf)
 end
